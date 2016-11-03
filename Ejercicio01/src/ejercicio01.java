53eef84d61a76f93845405ed7993e4689c301f6d
mariosoro <orosgomez@gmail.com>
2016-11-03 18:27:46 +0100

    Empezamos

---
+++ Ejercicio01/src/ejercicio01.java
@@ -0,0 +1,20 @@
+/*Escribir un programa que calcule e imprima la suma de los números enteros comprendidos 
+		  entre A y B que deben leerse por teclado y filtrarse para que A<=B. 
+		  Los valores de A y B no deben incluirse en la suma.*/
+
+import java.util.Scanner;
+public class ejercicio01 {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+	int a = sc.nextInt(), b = sc.nextInt();
+	
+	
+	System.out.println("Introduzca el numero A");
+		
+		
+	System.out.println("Introduzca el numero A");	
+		
+
+	}
+
+}
