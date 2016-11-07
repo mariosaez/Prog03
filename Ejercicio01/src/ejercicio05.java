78c4a8993943bba4ce255b4f5294ea74e1dd69fc
mariosoro <orosgomez@gmail.com>
2016-11-07 19:23:29 +0100

    en ello

---
+++ Ejercicio01/src/ejercicio05.java
@@ -0,0 +1,27 @@
+/*Hacer un programa que lea una serie de números por teclado de manera que cuando el número leído
+ * sea el cero ya no se introduzcan más, el programa debe calcular y escribir 
+ * la suma de los números de la serie y cuántos se han introducido en total.*/
+import java.util.Scanner;
+public class ejercicio05{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+	int  n,contador, suma; 
+	System.out.println("Introduzca el N1");
+	n = sc.nextInt();
+	contador=0;
+	suma=contador+n;
+	
+	while(n!=0){
+	System.out.println("Introduzca el N1");
+	n = sc.nextInt();
+	contador=n+1;
+	if(n==0){
+		
+		
+		
+	}
+	
+	
+	}
+ }
+}
