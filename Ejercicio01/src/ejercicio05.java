ebb3b5523edfd262b97d152478b2ccf1b867012b
MarioSoro <orosgomez@gmail.com>
2016-11-10 13:58:36 +0100

    terminados

--- Ejercicio01/src/ejercicio05.java
+++ Ejercicio01/src/ejercicio05.java
@@ -1,27 +1,33 @@
-/*Hacer un programa que lea una serie de números por teclado de manera que cuando el número leído
- * sea el cero ya no se introduzcan más, el programa debe calcular y escribir 
- * la suma de los números de la serie y cuántos se han introducido en total.*/
+/*Hacer un programa que lea una serie de números por teclado de manera 
+ * que cuando el número leído sea el cero ya no se introduzcan más, 
+ *el programa debe calcular y escribir la suma de los números de la serie 
+ *y cuántos se han introducido en total.*/
 import java.util.Scanner;
-public class ejercicio05{
+public class ejercicio05 {
+
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-	int  n,contador, suma; 
-	System.out.println("Introduzca el N1");
-	n = sc.nextInt();
-	contador=0;
-	suma=contador+n;
-	
-	while(n!=0){
-	System.out.println("Introduzca el N1");
-	n = sc.nextInt();
-	contador=n+1;
-	if(n==0){
+		// TODO Auto-generated method stub
+		Scanner teclado= new Scanner (System.in);
 		
+		int n,suma,contador;
 		
+		System.out.print ("Ingrese un numero:");
+		n = teclado.nextInt();
+		contador=0;
+		suma=n;
 		
+		while (n!=0) {
+			System.out.print ("Ingrese un numero:");
+			n = teclado.nextInt();
+				suma=n+suma;
+				contador=contador+1;
+			
+			
+			
+		}
+		
+		System.out.println("La suma de los numeros es: " + suma);
+		System.out.println("Has introducido: " + contador + " numeros");
 	}
-	
-	
-	}
- }
+
 }
---
+++ Ejercicio01/src/ejercicio06.java
@@ -0,0 +1,39 @@
+
+/*Hacer un programa que vaya leyendo números por el teclado hasta que lea el cero, que indicará
+el fin de la entrada de datos. El programa debe calcular y escribir 
+la suma de los números introducidos en lugar par y de los introducidos en lugar impar,
+es decir los introducidos primero, tercero, quinto, etc. y la suma de los introducidos segundo, cuarto, sexto, etc.*/
+import java.util.Scanner;
+public class ejercicio06 {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			Scanner t= new Scanner (System.in);
+	
+			int n,sp,si;
+			
+			
+			System.out.print ("Ingrese un numero:");
+			n = t.nextInt();
+			si=n;
+			
+			System.out.print ("Ingrese un numero:");
+			n = t.nextInt();
+			sp=n;
+			while (n!=0) {
+				
+				System.out.print ("Ingrese un numero:");
+				n = t.nextInt();
+				si=si + n;
+				
+				System.out.print ("Ingrese un numero:");
+				n = t.nextInt();
+				sp=sp + n;
+
+			
+		}
+			System.out.println(si);
+			System.out.println(sp);	
+		
+	}
+}
